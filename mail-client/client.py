7aac89aeff4dd1fcc81825fdf362edc887a6fbbe
liamzwbao <zhiv.bao@gmail.com>
2023-08-01 16:24:39 -0700

    Implement the mail client using Google Mail server

--- mail-client/client.py
+++ mail-client/client.py
@@ -1,54 +1,106 @@
+import base64
+import ssl
+import sys
+from datetime import datetime
 from socket import *
 
-msg = "\r\n I love computer networks!"
-end_msg = "\r\n.\r\n"
-
 # Choose a mail server (e.g. Google Mail server) and call it mailserver
-mailserver =  # Fill in start    #Fill in end
+mailserver = "smtp.gmail.com"
+mailserver_port = 587
+
+
+# Util function
+def send_command(command: str) -> str:
+    print(f"C: {command}")
+    client_socket.send(f"{command}\r\n".encode())
+    recv = client_socket.recv(1024).decode()
+    print(f"S: {recv}")
+    return recv
+
 
 # Create socket called client_socket and establish a TCP connection with mailserver
-# Fill in start
-
-# Fill in end
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((mailserver, mailserver_port))
 recv = client_socket.recv(1024).decode()
 print(recv)
 if recv[:3] != '220':
     print("220 reply not received from server.")
 
 # Send HELO command and print server response.
-helo_command = 'HELO Alice\r\n'
-client_socket.send(helo_command.encode())
-recv1 = client_socket.recv(1024).decode()
-print(recv1)
-if recv1[:3] != '250':
-    print("250 reply not received from server.")
+send_command("HELO Alice")
+
+# Parse mail info from user input
+username = input("Enter your gmail.com username: ")
+password = input("Enter your gmail.com password: ")
+rcpt = input("Enter the recipient's email address: ")
+name = input("Enter your name: ")
+subject = input("Enter the subject for your mail: ")
+
+# Allow the user to type multi-line messages
+print("Enter the message you want to send. Press Ctrl-D to stop.")
+message = []
+while True:
+    try:
+        line = input("")
+    except EOFError:
+        break
+    message.append(f"{line}\r\n")
+end_msg = "\r\n."
+
+# Start TLS
+recv = send_command("STARTTLS")
+if recv[:3] != '220':
+    print("220 reply not received from server.")
+    sys.exit(0)
+context = ssl.create_default_context()
+client_socket = context.wrap_socket(client_socket, server_hostname=mailserver)
+print("TLS AUTH success")
+
+# Authentication
+base64_str = f"\0{username}\0{password}".encode()
+base64_str = base64.b64encode(base64_str)
+auth_msg = f"AUTH PLAIN {base64_str.decode()}"
+recv = send_command(auth_msg)
+if recv[:3] == '503':
+    print("Already authenticated.")
+if recv[:3] != '235':
+    print("235 reply not received from server.")
+    sys.exit(0)
+print("ACCOUNT AUTH success")
 
 # Send MAIL FROM command and print server response.
-# Fill in start
-
-# Fill in end
+recv = send_command(f"MAIL FROM: <{username}>")
+if recv[:3] != '250':
+    print("250 reply not received from server.")
+    sys.exit(0)
 
 # Send RCPT TO command and print server response.
-# Fill in start
-
-# Fill in end
+recv = send_command(f"RCPT TO: <{rcpt}>")
+if recv[:3] != '250':
+    print("250 reply not received from server.")
+    sys.exit(0)
 
 # Send DATA command and print server response.
-# Fill in start
+recv = send_command("DATA")
+if recv[:3] != '354':
+    print("354 reply not received from server.")
+    sys.exit(0)
 
-# Fill in end
+# Send message header
+message_header = (f"From: {name} <{username}>\r\n"
+                  f"To: {rcpt}\r\n"
+                  f"Date: {datetime.now()}\r\n"
+                  f"Subject: {subject}\r\n")
 
 # Send message data.
-# Fill in start
-
-# Fill in end
+message_data = message_header
+for msg in message:
+    message_data += f"{msg}\r\n"
 
 # Message ends with a single period.
-# Fill in start
-
-# Fill in end
+send_command(message_data + end_msg)
 
 # Send QUIT command and get server response.
-# Fill in start
+send_command("QUIT")
 
-# Fill in end
+client_socket.close()
